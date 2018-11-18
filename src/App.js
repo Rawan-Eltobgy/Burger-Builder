d2bacc9fd13affc34ad9d565c024a3f827f3d2df
Eltobgy <rawaneltobgy@gmail.com>
2018-11-18 15:54:35 +0200

    Add dynamic ingredients

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <BurgerBuilder ingredients = {this.state.ingredients} />
         </Layout>
       </div>
     );
