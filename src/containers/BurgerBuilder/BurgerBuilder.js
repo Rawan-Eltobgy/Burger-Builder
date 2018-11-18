921e2664dd669b564030c110e0a33005070d3472
Eltobgy <rawaneltobgy@gmail.com>
2018-11-18 04:41:32 +0200

    Added burger style

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/_Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render(){
         return (
         <Aux>
-        <p>Hello</p>
+       <Burger/>
         </Aux>
         );
     }
