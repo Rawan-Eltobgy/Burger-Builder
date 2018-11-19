c06710786a2cda16e3985abd87d62179dd666403
Eltobgy <rawaneltobgy@gmail.com>
2018-11-19 13:38:25 +0200

    Add control buttons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) =>{
     //extracts the keys of the object and returns an array of keys
-    const transformedIngredients = Object.keys(props.ingredients).map(igkey =>{
+    let transformedIngredients = Object.keys(props.ingredients).map(igkey =>{
         return [...Array(props.ingredients[igkey])].map((_,i) => {
            return <BurgerIngredient key = {igkey + i} type={igkey} />
         });
-    });
+        //ewduce((prevVal, currVal))
+    }).reduce((arr, el)=> {
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p> Please start adding ingredients </p>
+    }
      return (
          <div className={classes.Burger}>
          <BurgerIngredient type = "bread-top" />
