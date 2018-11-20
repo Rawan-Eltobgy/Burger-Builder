6bcbd44ace1b903eb01d5e7e5d07eb4004fe54af
Eltobgy <rawaneltobgy@gmail.com>
2018-11-20 04:01:31 +0200

    Add order Summary *WIP*

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Aux from '../../../hoc/_Aux'
+//import classes from './Modal.css';
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (<li>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}:</span>
+                        {props.ingredients[igKey]}
+                    </li>);
+        }
+    );
+return(
+    <Aux>
+    <h3> <strong>Your Order</strong></h3>
+    <p>Your burger has the following ingredients: </p>
+    <ul>
+        {ingredientSummary}
+    </ul>
+    <p> Continue to check out? </p>
+    </Aux>
+);
+};
+    
+
+export default orderSummary;
